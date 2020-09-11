56fab7528cedf39576e0a41f008c52739a132e1a
claudio_curin <ccurinmiranda@gmail.com>
2020-09-11 19:54:31 -0300

    Formulario Postula

--- src/app/publica/guardias/guardias.component.ts
+++ src/app/publica/guardias/guardias.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faBellSlash, faHandPaper, faUser } from '@fortawesome/free-regular-svg-icons';
-import { faCoffee, faTrash, faTrashAlt, faPencilAlt, faTh } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee, faTrash, faTrashAlt, faPencilAlt, faTh, faCalendar, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { ActivatedRoute } from '@angular/router';
 
 
@@ -18,9 +18,12 @@ export class GuardiasComponent implements OnInit {
   faTrash = faTrash;
   faCofee = faCoffee;
   faTh = faTh;
+  faCalendar = faCalendar;
+  faCalendarAlt = faCalendarAlt;
 
   titulo: string;
   imagen: string;
+  curso: string;
 
   constructor(
     faConfig: FaConfig,
@@ -38,16 +41,19 @@ export class GuardiasComponent implements OnInit {
         case '1': {
           this.titulo = "Guardia de Seguridad";
           this.imagen = "http://www.diarioeldia.cl/sites/default/files/styles/flexslider_full/public/092019/ayer_a_nivel_nacional.jpg?itok=3eUNFSuZ";
+          this.curso = "OS-10";
           break;
         }
         case '2': {
           this.titulo = "Supervisor de Seguridad"; 
           this.imagen = "https://curso-os10.cl/wp-content/uploads/2019/01/supervisor-de-seguridad.png";
+          this.curso = "OS-10";
           break;
         }
         case '3': {
           this.titulo = "Conserje"; 
           this.imagen = "https://www.infogate.cl/wp-content/uploads/2019/04/empresas090401.jpg";
+          this.curso = "Mayordomo";
           break;
         }
         default: {
