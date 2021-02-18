d785303ea601911785577b45c5e555a787ace7ec
claudio_curin <ccurinmiranda@gmail.com>
2021-02-18 15:21:19 -0300

    parametrosServer_CCM

--- Server/ingEmpresa/getEmpresa.php
+++ Server/ingEmpresa/getEmpresa.php
@@ -4,10 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Content-type: text/html; charset=UTF-8');
-
 $bd = include_once "../bd.php";
 
-$sentencia = $bd->query("SELECT REGION_ID, REGION_NOMBRE FROM TREGION");
-$regiones = $sentencia->fetchAll(PDO::FETCH_OBJ);
-echo utf8_decode(json_encode($regiones));
+$sentencia = $bd->query("SELECT idEmpresa, rutEmpresa, nomEmpresa, rutRepresentante, nomRepresentante, dirEmpresa, telEmpresa, emailEmpresa, passEmpresa, estEmpresa FROM loginempresa where estEmpresa = 1");
+$empleos = $sentencia->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($empleos);
--- Server/parametros/getParametros.php
+++ Server/parametros/getParametros.php
@@ -6,8 +6,12 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Content-type: text/html; charset=UTF-8');
 
+if (empty($_GET["codgrupo"])) {
+    exit("No hay id de Grupo");
+}
+$codgrupo = $_GET["codgrupo"];
 $bd = include_once "../bd.php";
-
-$sentencia = $bd->query("SELECT REGION_ID, REGION_NOMBRE FROM TREGION");
-$regiones = $sentencia->fetchAll(PDO::FETCH_OBJ);
-echo utf8_decode(json_encode($regiones));
+$sentencia = $bd->query("SELECT pra_val, pra_des FROM parametros WHERE pra_grp_id = $codgrupo and pra_idr_vca = 1");
+$sentencia->execute();
+$parametros = $sentencia->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($parametros);
