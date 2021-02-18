1c80757a874d749fdd323f5d78786e52c54cb508
claudio_curin <ccurinmiranda@gmail.com>
2021-02-18 11:28:00 -0300

    20211802_1130_CCM

--- src/app/core/empresa/consultarempleado.ts
+++ src/app/core/empresa/consultarempleado.ts
@@ -1,24 +1,23 @@
 export class consultarempleado {
-   
-    constructor(
-             public nombreincisocorto: string,
-        public idEvaluacion:string,
-        public rutempleado:string,
-        public nombreempleado:string,
-        public rutempresa:string,
-        public nombreempresa:string,
-        public fechaingreso:string,
-        public fechatermino:string,
-        public idarticulo:string,
-        public nombrearticulo:string,
-        public Observacion:string,
-        public autorizacion:string,
-        public recomienda:string
 
-      
-  
-    
-    ){
-       }
-  }
-  
+    constructor(
+        public nombreincisocorto: string,
+        public idEvaluacion: string,
+        public rutempleado: string,
+        public nombreempleado: string,
+        public rutempresa: string,
+        public nombreempresa: string,
+        public fechaingreso: string,
+        public fechatermino: string,
+        public idarticulo: string,
+        public nombrearticulo: string,
+        public Observacion: string,
+        public autorizacion: string,
+        public recomienda: string
+
+
+
+
+    ) {
+    }
+}
