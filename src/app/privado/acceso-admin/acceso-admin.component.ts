bc8fae32055d148f1d3fa9d1b3ebf2c87783715e
claudio_curin <ccurinmiranda@gmail.com>
2021-02-24 00:34:06 -0300

    .

--- src/app/privado/acceso-admin/acceso-admin.component.ts
+++ src/app/privado/acceso-admin/acceso-admin.component.ts
@@ -25,6 +25,7 @@ export class AccesoAdminComponent implements OnInit {
 
     console.log("this.globals.rutEmpresa");
     console.log(this.globals.rutEmpresa);
+    
 
     if (this.globals.perfil == "1") {
       // this.role = this.apiservice.getToken();
