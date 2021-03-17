1d850a1b19ae1946b1eb8729f5707eec228bab31
claudio_curin <ccurinmiranda@gmail.com>
2021-03-17 13:18:03 -0300

    20210317_1317_CCM

--- src/app/encabezado/encabezado.component.ts
+++ src/app/encabezado/encabezado.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from '../services/login/api.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from './../core/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-encabezado',
@@ -18,6 +19,10 @@ export class EncabezadoComponent implements OnInit {
   perfilAdmin: boolean = false;
   perfilEmpresa: boolean = false;
   perfilPostulante: boolean = false;
+
+  nombre: string;
+
+  pruebasalida
 
   constructor(
     protected dataService: ApiService,
@@ -33,32 +38,43 @@ export class EncabezadoComponent implements OnInit {
 
   ngOnInit() {
 
-    this.dataService.getLoggedInName.subscribe(name => this.changeName(name));
+    this.perfilAdmin = false;
+    this.perfilEmpresa = false;
+    this.perfilPostulante = false;
 
+    this.dataService.getLoggedInName.subscribe(name => this.changeName(name));
     this.dataService.getLoggedInPerfil.subscribe(name => this.changePerfil(name));
 
-
+  
     this.globals.perfil = this.dataService.getPerfil();
     this.globals.email = this.dataService.getEmail();
     this.globals.rutEmpresa = this.dataService.getRutEmpresa();
 
-
-
     if (this.dataService.isLoggedIn()) {
+
       this.loginbtn = false;
       this.logoutbtn = true;
 
-      if (this.globals.perfil == "1") {
+      if (this.globals.perfil === "1") {
+        this.perfilEmpresa = false;
+        this.perfilPostulante = false;
         this.perfilAdmin = true;
       }
 
-      if (this.globals.perfil == "2") {
+      if (this.globals.perfil === "2") {
         this.perfilEmpresa = true;
+        this.perfilAdmin = false;
+        this.perfilPostulante = false;
       }
 
-      if (this.globals.perfil == "3") {
-        this.perfilPostulante= true;
+      if (this.globals.perfil === "3") {
+        this.perfilPostulante = true;
+        this.perfilAdmin = false;
+        this.perfilEmpresa = false;
       }
+
+
+
     }
     else {
       this.loginbtn = true;
@@ -67,42 +83,60 @@ export class EncabezadoComponent implements OnInit {
       this.perfilEmpresa = false;
       this.perfilPostulante = false;
     }
+
   }
 
 
   private changeName(name: boolean): void {
+    console.log("name");
+    console.log(name);
     this.logoutbtn = name;
     this.loginbtn = !name;
+    
+    this.globals.perfil = this.dataService.getPerfil();
 
-    if (this.globals.perfil == "1") {
+    if (this.globals.perfil === "1") {
+      this.perfilEmpresa = false;
+      this.perfilPostulante = false;
       this.perfilAdmin = true;
     }
 
-    if (this.globals.perfil == "2") {
+    if (this.globals.perfil === "2") {
       this.perfilEmpresa = true;
+      this.perfilAdmin = false;
+      this.perfilPostulante = false;
     }
 
-    if (this.globals.perfil == "3") {
+    if (this.globals.perfil === "3") {
       this.perfilPostulante = true;
+      this.perfilAdmin = false;
+      this.perfilEmpresa = false;
     }
   }
 
 
   private changePerfil(name: boolean): void {
+    console.log("name");
+    console.log(name);
+    this.globals.perfil = this.dataService.getPerfil();
 
-    if (this.globals.perfil == "1") {
-      this.perfilAdmin = true;
-    }
-
-    if (this.globals.perfil == "2") {
+    if (this.globals.perfil === "1") {
       this.perfilEmpresa = true;
+      this.perfilAdmin = false;
+      this.perfilPostulante = false;
     }
 
-    if (this.globals.perfil == "3") {
+    if (this.globals.perfil === "2") {
+      this.perfilEmpresa = true;
+      this.perfilAdmin = false;
+      this.perfilPostulante = false;
+    }
+
+    if (this.globals.perfil === "3") {
       this.perfilPostulante = true;
+      this.perfilAdmin = false;
+      this.perfilEmpresa = false;
     }
-
-    
   }
 
 
